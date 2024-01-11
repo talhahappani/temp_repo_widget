48378ba77a97852f2980b74233544277ea2a17aa
Talha Happani <talhahappani@hotmail.com>
2024-01-11 13:45:57 +0300

    widget clone

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// const carouselSlide = document.querySelector('.carousel-slide');
+// const carouselItem = document.querySelectorAll(".carousel-item")
+
+// console.log(carouselSlide);
+
+// // Buttons
+// const prevBtn = document.querySelector("#prevBtn");
+// const nextBtn = document.querySelector("#nextBtn");
+
+// // Counter
+// let counter = 1;
+// let size = carouselImages[0].clientWidth;
+// window.addEventListener('resize', () => {
+
+//     carouselSlide.style.transition = "none";
+
+//     size = carouselImages[0].clientWidth;
+
+//     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+// });
+
+
+
+
+// carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px'
+
+// // Button listeners
+// nextBtn.addEventListener('click', () => {
+//     carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+//     counter++;
+//     console.log(counter);
+// })
