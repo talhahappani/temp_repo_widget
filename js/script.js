dc80573b7a0f8d1ab617664ddb717da980b47a69
Talha Happani <talhahappani@hotmail.com>
2024-01-12 21:47:36 +0300

    Fixed all bugs, added swiper

--- js/script.js
+++ js/script.js
@@ -1,33 +1,30 @@
-// const carouselSlide = document.querySelector('.carousel-slide');
-// const carouselItem = document.querySelectorAll(".carousel-item")
-
-// console.log(carouselSlide);
-
-// // Buttons
-// const prevBtn = document.querySelector("#prevBtn");
-// const nextBtn = document.querySelector("#nextBtn");
-
-// // Counter
-// let counter = 1;
-// let size = carouselImages[0].clientWidth;
-// window.addEventListener('resize', () => {
-
-//     carouselSlide.style.transition = "none";
-
-//     size = carouselImages[0].clientWidth;
-
-//     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-// });
-
-
-
-
-// carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px'
-
-// // Button listeners
-// nextBtn.addEventListener('click', () => {
-//     carouselSlide.style.transition = 'transform 0.4s ease-in-out';
-//     counter++;
-//     console.log(counter);
-// })
+var swiper = new Swiper(".content", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 3,
+        },
+        700: {
+            slidesPerView: 2,
+            spaceBetween: 6,
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+    },
+  });
